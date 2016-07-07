d1c182c17d7af809bb894f67058b4aabaeb0e79a
Jim LAURIE <j.laurie6993@gmail.com>
2016-07-07 11:56:17 +0200

    Add inject css lib and build

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,40 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var inject = require('gulp-inject');
 
-gulp.task('serve', ['sass', 'min-css'], function() {
+gulp.task('serve', ['sass', 'min-css', 'inject'], function() {
   browserSync.init({
     server: './app'
   });
 
-  gulp.watch('app/scss/**/*.scss', ['sass', 'min-css']);
+  gulp.watch('app/styles/scss/**/*.scss', ['sass', 'min-css', 'inject']);
   gulp.watch('app/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/*.scss')
+  return gulp.src('app/styles/scss/main.scss')
     .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('app/styles/build'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('min-css', function() {
-  gulp.src('app/css/main.css')
+  gulp.src('app/styles/build/main.css')
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/styles/build'));
 });
 
 gulp.task('default', ['serve']);
+
+gulp.task('inject', function () {
+  var htmlFiles = gulp.src('app/pages/**/*.html');
+  var libs = gulp.src('app/styles/libs/**/*.css', {read: false}, {relative: true});
+  var build = gulp.src('app/styles/build/main.min.css', {read: false}, {relative: true});
+
+  return htmlFiles
+    .pipe(inject(libs, {starttag: '<!-- inject:libs:{{ext}} -->', ignorePath: 'app'}))
+    .pipe(inject(build, {starttag: '<!-- inject:build:{{ext}} -->', ignorePath: 'app'}))
+    .pipe(gulp.dest('app/pages'));
+});
