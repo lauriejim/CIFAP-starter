f27991b048f4df9bfaaba6dd7d77e7eb9a6aba69
Jim LAURIE <j.laurie6993@gmail.com>
2016-07-09 15:27:37 +0200

    Fix ortho

--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,19 @@ var inject = require('gulp-inject');
 gulp.task('default', ['serve']);
 
 // Tâche `serve`
-// Execute les tâches `saas`, `min-css` et `inject` avant se de lancer
+// Execute les tâches `saas`, `min-css` et `inject` avant de se lancer
 gulp.task('serve', ['sass', 'min-css', 'inject'], function() {
   // Lancement du serveur de synchronisation - Il va chercher l'application dans le dossier `app`
   browserSync.init({
     server: './app'
   });
 
-  // Dès qu'un fichier `scss` se trouvant dans le dossier `app/styles/scss` est modifier
-  // les tâche `saas`, `min-css` et `ìnject` sont éxecutées
+  // Dès qu'un fichier `scss` se trouvant dans le dossier `app/styles/scss` est modifié
+  // les tâches `saas`, `min-css` et `inject` sont éxecutées
   gulp.watch('app/styles/scss/**/*.scss', ['sass', 'min-css', 'inject']);
 
-  // Dès qu'un fichier `html` se trouvant dans le dossier `app` est modifier
-  // la page du navigateur se rafraichi pour afficher les modificcations
+  // Dès qu'un fichier `html` se trouvant dans le dossier `app` est modifié
+  // la page du navigateur se rafraichit pour afficher les modificcations
   gulp.watch('app/**/*.html').on('change', browserSync.reload);
 });
 
@@ -46,16 +46,16 @@ gulp.task('min-css', function() {
 });
 
 // Tâche `inject`
-// Inject automatiquement les fichiers `css` dans les pages `html`
+// Injection automatique des fichiers `css` dans les pages `html`
 gulp.task('inject', function () {
   // Récupération des fichiers `html`
   var htmlFiles = gulp.src('app/pages/**/*.html');
 
-  // Récupération des fichiers `css` - les `libs` et le fichier `main.min.css`
+  // Récupération des fichiers `css` - des `libs` et le fichier `main.min.css`
   var libs = gulp.src('app/styles/libs/**/*.css');
   var build = gulp.src('app/styles/build/main.min.css');
 
-  // Insertion des fichiers `css` dans les fichiers `html` entre les balises inject
+  // Insertion des fichiers `css` dans les fichiers `html` entre les balises `inject`
   return htmlFiles
     .pipe(inject(libs, {starttag: '<!-- inject:libs:{{ext}} -->', ignorePath: 'app'}))
     .pipe(inject(build, {starttag: '<!-- inject:build:{{ext}} -->', ignorePath: 'app'}))
